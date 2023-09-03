4f92be4b1a636f2833c6ce61d5ae284c15d1cdec
jeremiah <jeremiah>
2023-09-03 11:49:14 +0530

    chande 1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var urlToOpen = "http://127.0.0.1:5503/user.html";
+var urlToOpen = "https://jerom-physicus.github.io/sample-push/";
 
 // Use window.open to open the URL in the native browser
 window.open(urlToOpen, "_system");
