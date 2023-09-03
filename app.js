362871a342cba88cbedca2676e4cd3a26b97972d
jeremiah <jeremiah>
2023-09-03 11:59:28 +0530

    fe

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 var urlToOpen = "https://jerom-physicus.github.io/sample-push/";
 
-// Use window.open to open the URL in the native browser
-window.open(urlToOpen, "_system");
+// Try to open the URL in the default system browser
+var isChrome =
+  /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (isChrome) {
+  window.location.href = urlToOpen;
+} else {
+  // Fallback for non-Chrome browsers or devices without Chrome
+  // You can handle this case differently based on your requirements
+  alert("Please open this link in Chrome.");
+}
